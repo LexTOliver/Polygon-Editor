14b8d27b28521187a47ee9bad009c8b6e655cac7
LexTOliver <alext.oliver24@gmail.com>
2018-10-04 11:54:51 -0300

    Começando projeto
    Arquivo main configurando a interface de usuário

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+
+# GLOBAL VARIABLES
+display_resolution = (1200, 650)
+clock = pygame.time.Clock()
+
+# Window configuration for the editor
+def init_window():
+    pygame.init()
+    gameDisplay = pygame.display.set_mode(display_resolution)
+    pygame.display.set_caption('Polygon Editor - CG')
+
+# Running user interaction through window
+# Here we define the events with the user
+def run_window():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        pygame.display.update()
+        clock.tick(60)
+
+def main():
+    init_window()
+    run_window()
+
+if __name__ == '__main__':
+    main()
